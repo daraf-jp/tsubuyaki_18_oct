057f2da684415b609cb677c6285521fa1cda0b7b
Ken Ohata <ohata@daraf.co.jp>
2014-10-18 20:56:32 +0900

    define favorites#create

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,14 @@
 class FavoritesController < ApplicationController
+  before_filter :require_login
+
+  def create
+    @tweet    = Tweet.find(params[:tweet_id])
+    @favorite = current_user.favorites.build(tweet: @tweet)
+
+    if @favorite.save
+      redirect_to tweets_url, notice: "succesfully favorited."
+    else
+      redirect_to tweets_url, notice: "failed"
+    end
+  end
 end
