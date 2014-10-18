7c5c15e42941085ee320ee2254c9485e048b6bbb
Ken Ohata <ohata@daraf.co.jp>
2014-10-18 20:49:28 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018112347) do
+ActiveRecord::Schema.define(version: 20141018114850) do
+
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "tweet_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "favorites", ["tweet_id"], name: "index_favorites_on_tweet_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
 
   create_table "tweets", force: true do |t|
     t.integer  "user_id"
