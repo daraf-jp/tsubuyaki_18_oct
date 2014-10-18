8d495c4ee144d068344dfc5e0574dc5b5e5cd628
Ken Ohata <ohata@daraf.co.jp>
2014-10-18 21:42:43 +0900

    define follows#create

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,14 @@
 class FollowsController < ApplicationController
+  before_filter :require_login
+
+  def create
+    @user = User.find(params[:user_id])
+    @follow = @user.follows.build(follower: current_user)
+
+    if @follow.save
+      redirect_to tweets_url, notice: "successfully followed."
+    else
+      redirect_to tweets_url
+    end
+  end
 end
