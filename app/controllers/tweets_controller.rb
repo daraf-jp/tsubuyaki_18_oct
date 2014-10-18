aadb9665b3eef5321bc02bdf8aa9ea632ace5142
Ken Ohata <ohata@daraf.co.jp>
2014-10-18 20:32:26 +0900

    update tweets#create

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -26,6 +26,7 @@ class TweetsController < ApplicationController
   # POST /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
 
     respond_to do |format|
       if @tweet.save
@@ -70,6 +71,6 @@ class TweetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tweet_params
-      params.require(:tweet).permit(:user_id, :content)
+      params.require(:tweet).permit(:content)
     end
 end
