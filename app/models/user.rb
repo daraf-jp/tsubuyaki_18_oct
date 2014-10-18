71ff891531be69e61ab8d54bd38d7aeb18b19fda
Ken Ohata <ohata@daraf.co.jp>
2014-10-18 21:15:50 +0900

    assoc Follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
 
   has_many :tweets, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follows, foreign_key: :inverse_follower_id
+  has_many :followers, through: :follows
+  has_many :inverse_follows, foreign_key: :follower_id, class_name: Follow
+  has_many :inverse_followers, through: :inverse_follows
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-z][a-z0-9]+\z/ }
   validates :email, presence: true, uniqueness: true
