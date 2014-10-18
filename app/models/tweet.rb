64bfae6bbbcbab6cf49e68f6523b5af3b602c61d
Ken Ohata <ohata@daraf.co.jp>
2014-10-18 20:58:47 +0900

    define Tweet#favorited_by?

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,8 @@ class Tweet < ActiveRecord::Base
 
   validates :user, presence: true
   validates :content, presence: true, length: { in: 1..140 }
+
+  def favorited_by? user
+    favorites.where(user_id: user.id).exists?
+  end
 end
