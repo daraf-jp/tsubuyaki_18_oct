ebdae06bb75a936f008ba022cc96b3d60d447d5b
Ken Ohata <ohata@daraf.co.jp>
2014-10-18 19:36:51 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-z][a-z0-9]+\z/ }
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true, if: :password
 end
