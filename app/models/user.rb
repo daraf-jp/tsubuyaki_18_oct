9c77569c2bf1f52d6f7597b455bc468e3fb11ef8
Ken Ohata <ohata@daraf.co.jp>
2014-10-18 20:52:57 +0900

    assoc Favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   has_many :tweets, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-z][a-z0-9]+\z/ }
   validates :email, presence: true, uniqueness: true
