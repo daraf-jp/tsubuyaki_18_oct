47bee5512b1e5e1f6849d91130b49b21284e6437
Ken Ohata <ohata@daraf.co.jp>
2014-10-20 16:44:31 +0900

    define link_to_follow_or_unfollow

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def link_to_follow_or_unfollow user
+    if user.followed_by? current_user
+      link_to "フォロー解除", user_follows_path(user), method: :delete
+    else
+      link_to "フォローする", user_follows_path(user), method: :post
+    end
+  end
 end
