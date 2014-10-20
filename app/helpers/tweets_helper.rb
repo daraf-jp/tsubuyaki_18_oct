df758b94db6e9c09417b9e6ecebe676d3633759e
Ken Ohata <ohata@daraf.co.jp>
2014-10-20 16:46:13 +0900

    define link_to_favorite_or_unfavorite

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,9 @@
 module TweetsHelper
+  def link_to_favorite_or_unfavorite tweet
+    if tweet.favorited_by? current_user
+      link_to "お気に入りを解除", tweet_favorites_path(tweet), method: :delete
+    else
+      link_to "お気に入りに追加", tweet_favorites_path(tweet), method: :post
+    end
+  end
 end
