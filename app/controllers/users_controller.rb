7913a7248651368e296a0b1d2a9b66ffca9132ad
Ken Ohata <ohata@daraf.co.jp>
2014-10-20 20:21:33 +0900

    add users#favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :favorites, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+  end
+
+  def favorites
   end
 
   # GET /users/new
