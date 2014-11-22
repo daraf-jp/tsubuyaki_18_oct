73875d16faae1cd06bc9819ddda0373b434e5938
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 14:37:31 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018121313) do
+ActiveRecord::Schema.define(version: 20141122052924) do
 
   create_table "favorites", force: true do |t|
     t.integer  "user_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20141018121313) do
     t.string   "salt",             null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "screen_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
