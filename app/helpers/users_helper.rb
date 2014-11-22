5a1800bc2ef4d11a947394744cd15774cc3c05c6
Keito Teranishi <teranishi@undapt.jp>
2014-11-22 19:12:11 +0900

    トップの背景問題解決

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,10 @@
 module UsersHelper
   def render_user_screen_name user
-    user.screen_name or user.name
+    if user.screen_name.nil? or user.screen_name.empty?
+      user.name
+    else
+      user.screen_name
+    end
   end
 
   def link_to_follow_or_unfollow user
