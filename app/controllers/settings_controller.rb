3d8b8d0a7f7ba39f0d4e86bc49189f8ca2a1a45d
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 14:53:44 +0900

    update settings#edit

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,4 +1,21 @@
 class SettingsController < ApplicationController
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+
+    if @user.update_attributes(params_user)
+      redirect_to edit_settings_url, notice: "保存しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:name, :screen_name, :bio)
   end
 end
